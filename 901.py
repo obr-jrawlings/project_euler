4cd5f0ee7027fa1de35539990b696654a5869a6a
Joshua Rawlings <joshua.rawlings@outlook.com>
2024-11-20 22:36:02 +0000

    New attempts at 901

--- 901.py
+++ 901.py
@@ -1,44 +1,18 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Simulation parameters
-num_simulations = 1000
-increments = 0.1
-depth = np.arange(1, 15, increments).tolist()[1:]
-costs = []
-dict_cost = {}
 
-for d in depth:
+def objective(t):
+    n = len(t)
+    cost = t[0]
+    for i in range(1, n):
+        cost += np.exp(-t[i - 1]) * t[i]
+    return cost
 
-    for _ in range(num_simulations):
-        _d = d
-        prbsuc = 1 - math.exp(-_d)
-        cost = 0
-        while True:
-            if np.random.rand() < prbsuc:
-                break
-            else:
-                cost += _d
-                _d = _d + increments
-                prbsuc = 1 - math.exp(-_d)
-        if cost == 0:
-            cost = _d
-        else:
-            cost = cost + _d
-        costs.append(cost)
-    dict_cost[d] = costs
-    costs = []
 
-"""
-for key, value in dict_cost.items():
-    print(
-        f"Key: {key} -> Value Type: {type(value)}, Example Value: {value[:3] if isinstance(value, list) else value}"
-    )
-"""
-a = []
-for key, value in dict_cost.items():
-    print(key)
-    a.append(key - np.average(value))
-plt.plot(a)
+lists = []
+for x in range(2, 25):
+    lists.append(objective(list(range(x))))
+print(lists)
+plt.plot(lists)
 plt.show()
